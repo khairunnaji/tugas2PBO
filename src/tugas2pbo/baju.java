f35f3f0e6298ef1231d78ae3819606ff9aeeca1b
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2018-03-29 15:06:54 +0800

    Membuat Atribut Dan Beavior

--- src/tugas2pbo/baju.java
+++ src/tugas2pbo/baju.java
@@ -1,5 +1,13 @@
 
 package tugas2pbo;
 public class baju {
+    double Jumlah;
+    double Harga;
     
+    void cetaknfo(){
+        System.out.println("------------------------");
+        System.out.println("Jumlah : "+Jumlah);
+        System.out.println("Harga : "+Harga);
+        System.out.println("------------------------");
+    }
 }
